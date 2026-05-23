5d2a5f9ec7914e7c597e69413a0a640fbcd39693
辄咎 <17098243+ZhejiuW@user.noreply.gitee.com>
2026-05-23 18:35:13 +0800

    后端初始化

---
+++ backend/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampusSecondHand.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public string Hello()
+        {
+            return "Hello, 校园二手交易平台！";
+        }
+    }
+}
