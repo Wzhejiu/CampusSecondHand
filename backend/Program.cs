9195205fe6a256632f5b3e28e6dbb7c6d4e47313
辄咎 <17098243+ZhejiuW@user.noreply.gitee.com>
2026-05-25 16:06:22 +0800

    后端

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// 添加 CORS 策略（放在 AddControllers 之前或之后都可以）
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// 使用 CORS（放在 UseAuthorization 之前）
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
