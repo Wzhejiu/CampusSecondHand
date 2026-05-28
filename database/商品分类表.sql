8d3979007fa97958cfb99a62e957b5ed0fb58db6
MR.HHJ <17100595+hhj_1234@user.noreply.gitee.com>
2026-05-28 08:22:12 +0000

    rename database/商品分类表 to database/商品分类表.sql.
    Signed-off-by: MR.HHJ <17100595+hhj_1234@user.noreply.gitee.com>

---
+++ database/商品分类表.sql
@@ -0,0 +1,15 @@
+USE second_hand_trade;
+
+-- 一级分类
+INSERT INTO `category` (`category_name`, `parent_id`) VALUES 
+('教材书籍', NULL),
+('数码产品', NULL),
+('生活用品', NULL),
+('服饰鞋包', NULL),
+('代步工具', NULL),
+('运动器材', NULL),
+('美妆护肤', NULL),
+('宿舍家具', NULL),
+('考研考证', NULL),
+('其他闲置', NULL);
+
